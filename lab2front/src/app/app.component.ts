b67f298844232ddfe9ec28dcc3fff6e1bbdccc2f
OleksandrHorbun <90098607+OleksandrHorbun@users.noreply.github.com>
2022-06-11 20:01:13 +0300

    Changed TS

--- lab2front/src/app/app.component.ts
+++ lab2front/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Weather } from './weather';
+
+import { MainLogicService } from './main-logic.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'lab2front';
-}
+
+  title:string="lab2front";
+  itemsList:Weather[]=[];
+  showForms:boolean=false;
+  selectedItem?:Weather;
+
+  id:number = 0;
+
+  constructor(private service:MainLogicService) { }
+
+  ngOnInit(): void {
+    this.updateItems();
+  }
+
+  updateItems(){
+    this.service.getItems().subscribe(
+      (items)=>{
+        this.itemsList=items;
+        this.service.setList(items);
+      }
+    );
+  }
+
+  addItem(item:Weather){
+    this.service.postItem(item).subscribe(
+      (item)=>{
+        // console.log(item);
+        this.updateItems();
+      }
+    );
+  }
+
+  updateItem(item:Weather){
+    
+      this.service.putItem(item).subscribe(
+      ()=>{
+        this.updateItems();      
+      }
+    );
+
+  }
+
+  onSelect(item:Weather){
+    console.log(item);
+    if(this.selectedItem && item.id==this.selectedItem.id){
+      this.selectedItem=undefined;
+    } else {
+      this.selectedItem=item;
+    }
+  }
+
+  deleteItem(item:Weather){
+    this.service.deleteItem(item).subscribe(
+      ()=>{
+        this.updateItems();
+
+      }
+    );
+  }
+  }
--- lab2front/src/app/weather.ts
+++ lab2front/src/app/weather.ts
@@ -2,11 +2,11 @@ export interface Weather {
 
   id:number;
   city:string;
-	date:string;
-	temp:number; //double
-	temp_min:number; //double
-	temp_max:number; //double
-	feels_like:number; //double
-	description:string;
+  date:string;
+  temp:number; //double
+  temp_min:number; //double
+  temp_max:number; //double
+  feels_like:number; //double
+  description:string;
 
 }
