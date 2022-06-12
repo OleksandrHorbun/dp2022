4776938fc32ed800dced30b6e31f75dc27933c7c
OleksandrHorbun <90098607+OleksandrHorbun@users.noreply.github.com>
2022-06-12 05:45:20 +0300

    Add files via upload

---
+++ Student6.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE student6;
+create user oleksandr with password 'Secret';
+CREATE TABLE "weather" ("id" serial PRIMARY KEY, "city" varchar, "date" varchar, "temp" double precision, "temp_min" double precision, "temp_max" double precision, "feels_like" double precision, "description" varchar);
+grant all on database student6 to oleksandr;
